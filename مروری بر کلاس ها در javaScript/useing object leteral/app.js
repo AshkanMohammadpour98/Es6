a4b62fab2f7f5e41684dfbdd9671036eccf940b7
ashkan Mohammadpour <ashkanmohammadpour29@gmail.com>
2023-11-28 08:09:14 +0330

    add all coding object literal

---
+++ مروری بر کلاس ها در javaScript/useing object leteral/app.js
@@ -0,0 +1,27 @@
+//ابجکت ها مقدار های کلید مقدار راند میتونیم داخلشون متد و حتی
+//  ابجکت های توردر تو هم داشته باشیم
+
+//useing object leteral => معمولا از این روش  استفاده میشه
+const person = {
+    name : "milad",
+    lastName : "karime",
+    age : 28,
+    coding : function(){
+        console.log("milad is coding");
+    },
+    skills : {
+        html : 100 ,
+        css : 90 ,
+        js : 80 ,
+        php : 80
+    }
+
+}
+
+console.log(person);
+console.log(typeof person);
+console.log(person.name);
+console.log(person["name"]);
+console.log(person.coding);
+console.log(person.skills);
+console.log(person.skills.css);
